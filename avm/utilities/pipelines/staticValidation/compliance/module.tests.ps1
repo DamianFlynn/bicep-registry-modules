0297f46752a4b1f1fb2a36ab22b9d67f06ac4f96
Damian Flynn <info@damianflynn.com>
2024-04-29 11:08:16 +0100

    fix: turn off orphan check

--- avm/utilities/pipelines/staticValidation/compliance/module.tests.ps1
+++ avm/utilities/pipelines/staticValidation/compliance/module.tests.ps1
@@ -97,49 +97,49 @@ Describe 'File/folder tests' -Tag 'Modules' {
       $file.Name | Should -BeExactly 'README.md'
     }
 
-    It '[<moduleFolderName>] Module should contain a [` ORPHANED.md `] file only if orphaned.' -TestCases ($moduleFolderTestCases | Where-Object { $_.isTopLevelModule }) {
+    # It '[<moduleFolderName>] Module should contain a [` ORPHANED.md `] file only if orphaned.' -TestCases ($moduleFolderTestCases | Where-Object { $_.isTopLevelModule }) {
 
-      param(
-        [string] $moduleFolderPath
-      )
+    #   param(
+    #     [string] $moduleFolderPath
+    #   )
 
-      $templateFilePath = Join-Path -Path $moduleFolderPath 'main.bicep'
+    #   $templateFilePath = Join-Path -Path $moduleFolderPath 'main.bicep'
 
-      # Use correct telemetry link based on file path
-      $telemetryCsvLink = $moduleFolderPath -match '[\\|\/]res[\\|\/]' ? $telemetryResCsvLink : $telemetryPtnCsvLink
+    #   # Use correct telemetry link based on file path
+    #   $telemetryCsvLink = $moduleFolderPath -match '[\\|\/]res[\\|\/]' ? $telemetryResCsvLink : $telemetryPtnCsvLink
 
-      # Fetch CSV
-      # =========
-      try {
-        $rawData = Invoke-WebRequest -Uri $telemetryCsvLink
-      } catch {
-        $errorMessage = "Failed to download telemetry CSV file from [$telemetryCsvLink] due to [{0}]." -f $_.Exception.Message
-        Write-Error $errorMessage
-        Set-ItResult -Skipped -Because $errorMessage
-      }
-      $csvData = $rawData.Content | ConvertFrom-Csv -Delimiter ','
+    #   # Fetch CSV
+    #   # =========
+    #   try {
+    #     $rawData = Invoke-WebRequest -Uri $telemetryCsvLink
+    #   } catch {
+    #     $errorMessage = "Failed to download telemetry CSV file from [$telemetryCsvLink] due to [{0}]." -f $_.Exception.Message
+    #     Write-Error $errorMessage
+    #     Set-ItResult -Skipped -Because $errorMessage
+    #   }
+    #   $csvData = $rawData.Content | ConvertFrom-Csv -Delimiter ','
 
-      $moduleName = Get-BRMRepositoryName -TemplateFilePath $templateFilePath
-      $relevantCSVRow = $csvData | Where-Object {
-        $_.ModuleName -eq $moduleName
-      }
+    #   $moduleName = Get-BRMRepositoryName -TemplateFilePath $templateFilePath
+    #   $relevantCSVRow = $csvData | Where-Object {
+    #     $_.ModuleName -eq $moduleName
+    #   }
 
-      if (-not $relevantCSVRow) {
-        $errorMessage = "Failed to identify module [$moduleName]."
-        Write-Error $errorMessage
-        Set-ItResult -Skipped -Because $errorMessage
-      }
-      $isOrphaned = [String]::IsNullOrEmpty($relevantCSVRow.PrimaryModuleOwnerGHHandle)
+    #   if (-not $relevantCSVRow) {
+    #     $errorMessage = "Failed to identify module [$moduleName]."
+    #     Write-Error $errorMessage
+    #     Set-ItResult -Skipped -Because $errorMessage
+    #   }
+    #   $isOrphaned = [String]::IsNullOrEmpty($relevantCSVRow.PrimaryModuleOwnerGHHandle)
 
-      $orphanedFilePath = Join-Path -Path $moduleFolderPath 'ORPHANED.md'
-      if ($isOrphaned) {
-        $pathExisting = Test-Path $orphanedFilePath
-        $pathExisting | Should -Be $true -Because 'The module is orphaned.'
-      } else {
-        $pathExisting = Test-Path $orphanedFilePath
-        $pathExisting | Should -Be $false -Because ('The module is not orphaned but owned by [{0}].' -f $relevantCSVRow.PrimaryModuleOwnerGHHandle)
-      }
-    }
+    #   $orphanedFilePath = Join-Path -Path $moduleFolderPath 'ORPHANED.md'
+    #   if ($isOrphaned) {
+    #     $pathExisting = Test-Path $orphanedFilePath
+    #     $pathExisting | Should -Be $true -Because 'The module is orphaned.'
+    #   } else {
+    #     $pathExisting = Test-Path $orphanedFilePath
+    #     $pathExisting | Should -Be $false -Because ('The module is not orphaned but owned by [{0}].' -f $relevantCSVRow.PrimaryModuleOwnerGHHandle)
+    #   }
+    # }
   }
 
   Context 'Top level module folder tests' {
